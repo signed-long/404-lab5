15b22e95abc2a853da9804f5cac1a47f1f4ab6ce
signed-long <mrl@ualberta.ca>
2022-10-03 21:11:53 -0600

    update site

---
+++ content/article-2.md
@@ -0,0 +1,5 @@
+Title: My Second Article
+Date: 2019-01-07 8:00
+Category: Blog
+
+I really like free static web hosting.
--- publishconf.py
+++ publishconf.py
@@ -7,11 +7,12 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://signed-long.github.io/404-lab5/'
 RELATIVE_URLS = False
+THEME = 'templates/mytheme'
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
